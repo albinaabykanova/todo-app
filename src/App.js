0e07d28673676e8088ba0c2cc978180e4df3e06e
albinaabykanova <abykanovaalbina@gmail.com>
2023-05-18 23:40:26 +0300

    todo list daily comit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Content from "./components/Content";
 import Header from "./components/Header";
+import { TodoProvider } from "./context/TodoContext";
 
 
 function App() {
   return (
+    <TodoProvider>
     <section className="todoapp">
         <Header />
         <Content />
     </section>
+    </TodoProvider>
   );
 }
 
---
+++ src/context/TodoContext.js
@@ -0,0 +1,29 @@
+import  React, { createContext, useState, useContext} from "react";
+
+const TodoContext = createContext();
+
+export const TodoProvider = ({children}) => {
+    const [todos, setTodos] = useState([
+    {
+        id: 1,
+        text: "Learn React",
+        completed: false,
+    },
+    ]);
+
+    const values = {
+        todos,
+        setTodos,
+    };
+
+    return <TodoContext.Provider value={values}>{children}</TodoContext.Provider>
+};
+
+export const useTodo = () => {
+  const context = useContext(TodoContext);
+
+  if (context === undefined) {
+    throw new Error("useTodo hook must be call inside TodoProvider componen")
+  }
+  return context;
+}
