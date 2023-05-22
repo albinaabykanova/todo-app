2f654bd143b33aea6d5306bbb873994fe19ead2d
albinaabykanova <abykanovaalbina@gmail.com>
2023-05-22 22:46:57 +0300

    continue todo-app

--- src/components/Content/List/index.js
+++ src/components/Content/List/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import { useTodo} from "../../../context/TodoContext";
 import Item from "./Item";
 
+let filtred = null;
+
 function List() {
   const {todos} = useTodo();
+
+  filtred = todos;
+
+  if(activeFilter !== 'all')
 
     return (
       <ul className="todo-list">
